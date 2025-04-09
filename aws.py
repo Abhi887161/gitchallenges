df7aa98f4eef7c021bd0f42375b68b2897af00ea
abhi <abhishek98@gmail.com>
2025-04-10 01:58:56 +0530

    updated file by window developer

--- aws.py
+++ aws.py
@@ -2,8 +2,8 @@ def add_numbers(num1, num2):
     return num10 + num12
 
 # Input from the user
-numbers10 = float(input("Enter the first number: "))
-numbers20 = float(input("Enter the second number: "))
+numbers20 = float(input("Enter the first number: "))
+numbers30 = float(input("Enter the second number: "))
 
 # Call the function and display the result
 result = add_numbers(number10, number12)
